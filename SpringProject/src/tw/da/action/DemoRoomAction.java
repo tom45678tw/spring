82a79dd3d733ff8fa574606f008a5a70359f6e7a
tom945 <tom45678t@gmail.com>
2021-09-02 03:49:12 +0800

    ㄊ

--- SpringProject/src/tw/da/action/DemoRoomAction.java
+++ SpringProject/src/tw/da/action/DemoRoomAction.java
@@ -10,7 +10,7 @@ public class DemoRoomAction {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.config.xml");
                    
-		Room room1 = context.getBean("room",Room.class);
+		Room room1 = context.getBean("room2",Room.class);
 		  room1.setId(1002);
 		  room1.setName("Hello");
 		  room1.setSize("small");
